3a729d0be2ff535fd7ca34f98b4c5741ff1a7873
Trevor Nederlof <tnederlof@gmail.com>
2022-06-21 11:22:28 -0400

    Test2

--- content-dir/app.R
+++ content-dir/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data22232"),
+    titlePanel("Old Faithful Geyser Data222232"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
